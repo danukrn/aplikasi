242aa7318b07723672c2b582da4f9da07c177c9f
danukrn <56217729+danukrn@users.noreply.github.com>
2020-07-18 16:33:44 +0700

    Update MainActivity.kt
    val  b

--- app/src/main/java/com/helloapps/cobagitrepo/MainActivity.kt
+++ app/src/main/java/com/helloapps/cobagitrepo/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 val a = 1234
+val b = 3
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
